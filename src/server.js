18ceba627938855579c51275d5cb3c5bd5fde11d
Kevin Kamto <kamtokevin@gmail.com>
2020-12-09 17:05:07 -0500

    chore: mysql connection setup

--- src/server.js
+++ src/server.js
@@ -3,9 +3,41 @@ import mysql from 'mysql';
 
 // instantiations
 
+init()
+{
+    const { host, port, user, password, database } = {
+        host: 'mysql',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'master'
+      };
+
+      const connection = mysql.createConnection({
+        host,
+        port,
+        user,
+        password,
+        database
+      });
+}
 
 // MySQL Connection
 
+function makeQuery(query)
+{
+    connection.connect();
+
+    connection.query(query, (err, rows, fields) => {
+      if (err) res.send(err);
+  
+      res.send(rows);
+    });
+  
+    connection.end();
+}
 
 // Setup of files
+
+init();
 
